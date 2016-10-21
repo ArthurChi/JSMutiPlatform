7bc07bebf3d234cee0dc37ad161612153b4d2c5e
cjfire <cjfire@sina.cn>
2016-10-21 10:05:23 +0800

    project initialization

---
+++ JSMutiPlatform/JSMutiPlatform/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSMutiPlatform
+//
+//  Created by cjfire on 16/10/21.
+//  Copyright © 2016年 cjfire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSMutiPlatform/JSMutiPlatform/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JSMutiPlatform
+//
+//  Created by cjfire on 16/10/21.
+//  Copyright © 2016年 cjfire. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
